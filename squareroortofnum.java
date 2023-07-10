50372b0ccf289d463e5b1b98cb4cea5e3e468c4d
singamsai2002 <112846532+singamsai2002@users.noreply.github.com>
2023-07-10 19:30:39 +0530

    Create squareroortofnum.java

---
+++ squareroortofnum.java
@@ -0,0 +1,13 @@
+package sai.java;
+import java.util.*;
+public class squarerootofnum {
+  public static void main(String[] args) {
+	  System.out.print("Enter the number:");
+	  Scanner a = new Scanner(System.in);
+	  double n = a.nextInt();
+	  double sqrt=Math.pow(n,0.5);
+	  double sq =Math.sqrt(n);
+	  System.out.println(sqrt+" "+"-"+sqrt);
+	  
+  }
+}
