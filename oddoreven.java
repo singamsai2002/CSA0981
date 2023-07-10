4a0fb9bad2f47906312b47534125f807b60859d4
singamsai2002 <112846532+singamsai2002@users.noreply.github.com>
2023-07-10 19:24:29 +0530

    Create oddoreven.java

---
+++ oddoreven.java
@@ -0,0 +1,16 @@
+package sai.java;
+import java.util.*;
+public class oddoreven {
+	public static void main(String[] args) {
+		System.out.println("Enter the number:");
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		if(i%2==0) 
+			System.out.println(i +"is even");
+			else 
+				System.out.println(i +"it is  odd number");
+			
+		
+	}
+
+}
