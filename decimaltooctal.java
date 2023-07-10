881b83241ffbdb30a4e3ff0b52258537ba2726a9
singamsai2002 <112846532+singamsai2002@users.noreply.github.com>
2023-07-10 19:15:31 +0530

    Create decimaltooctal.java

---
+++ decimaltooctal.java
@@ -0,0 +1,16 @@
+package sai.java;
+import java.util.*;
+public class decimaltonumber {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		float n = sc.nextFloat();
+		float a,b;
+		a = n*n;
+		b=n*n*n;
+		System.out.println("the squaref the number:"+a);
+		System.out.println("the cube of the number:"+b);
+		
+	}
+
+}
